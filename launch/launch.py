68d0381937627e83a957b7c463eb18941f00d781
Xinjue <hit_zou@126.com>
2026-02-03 13:08:52 +0800

    revise to support namespace

--- launch/launch.py
+++ launch/launch.py
@@ -1,21 +1,60 @@
+# Copyright 2025 WheelHub Intelligent
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
-import os
+from launch_ros.descriptions import ParameterFile
+from nav2_common.launch import RewrittenYaml
 
 def generate_launch_description():
-    config = os.path.join(
-        get_package_share_directory('whi_temperature_humidity'),
+    # Input parameters declaration
+    namespace = LaunchConfiguration('namespace')
+
+    # Declare arguments
+    declare_namespace_arg = DeclareLaunchArgument(
+        'namespace', default_value='',
+        description='Top-level namespace'
+    )
+
+    # Path to config file
+    config_file = PathJoinSubstitution([
+        FindPackageShare('whi_temperature_humidity'),
         'config',
         'config.yaml'
+    ])
+
+    configured_params = ParameterFile(
+        RewrittenYaml(
+            source_file=config_file,
+            root_key=namespace,
+            param_rewrites={},
+            convert_types=True,
+        ),
+        allow_substs=True,
     )
 
     return LaunchDescription([
+        declare_namespace_arg,
         Node(
             package='whi_temperature_humidity',
             executable='whi_temperature_humidity_node',
             name='whi_temperature_humidity',
+            namespace=namespace,
             output='screen',
-            parameters=[config]
+            parameters=[configured_params]
         )
     ])
